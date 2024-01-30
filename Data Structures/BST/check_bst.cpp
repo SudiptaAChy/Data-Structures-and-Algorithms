fc574d6e2d63a05187eb1251e42c0693e4c893e2
Sudipta-BS23 <anikc795@gmail.com>
2024-01-31 00:33:00 +0600

    updated check bst

--- Data Structures/BST/check_bst.cpp
+++ Data Structures/BST/check_bst.cpp
@@ -8,16 +8,36 @@ using namespace std;
 
 struct node
 {
-    int value;
-    node *left, *right;
+    int val;
+    node* left, * right;
     node(int d)
     {
-        value = d;
+        val = d;
         left = NULL;
         right = NULL;
     }
 };
-node *root = NULL;
+node* root = NULL;
+
+bool checkBst1(node* root, int mn = INT_MIN, int mx = INT_MAX) {
+    if (root == NULL) return true;
+
+    if (root->val <= mn || root->val >= mx) return false;
+
+    return checkBst1(root->left, mn, root->val) & checkBst1(root->right, root->val, mx);
+}
+
+bool checkBst2(node* root, int prev = INT_MIN) {
+    if (root == NULL) return true;
+
+    if (!checkBst2(root->left, prev)) return false;
+
+    if (root->val <= prev) return false;
+
+    if (!checkBst2(root->right, root->val)) return false;
+
+    return true;
+}
 
 int main()
 {
